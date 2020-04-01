00e979266aac5ed9311b8ffe9842acdb4b18d9ab
Nikita Kuprin <nikon5070@gmail.com>
2020-04-01 12:45:32 +1000

    ci/cd - helm

--- app.js
+++ app.js
@@ -4,4 +4,4 @@ const port = 3000
 
 app.get('/', (req, res) => res.send('nikita'))
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`AAAAAAA Example app listening on port ${port}!`))
